d9c5906b4758a30d252e2f35d9f19e65d93741d9
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-04-06 09:34:36 -0500

    ooh a secret

---
+++ games/5.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400,400);
+    background(255);
+}
+function mousePressed() {
+    stroke("green");
+    fill("green");
+    circle(mouseX,mouseY,50);
+}
+
+//this game is made using p5.js
+//made by 7Zeb
